4bbfd4c7b22e15a0ca1744287ea4872e7a377f1b
jessi <jessi.nowski@hotmail.de>
2024-02-22 16:19:20 +0100

    added Frontende

---
+++ src/main/java/com/github/springrecap/SpringRecapApplication.java
@@ -0,0 +1,13 @@
+package com.github.springrecap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRecapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRecapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/springrecap/SpringRecapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.springrecap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
